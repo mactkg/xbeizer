77c75940f4686d1b5d51e906d0d53e97ec13e705
mactkg <mactkg@gmail.com>
2012-02-10 10:54:33 +0900

    Initial Commit

---
+++ xbeizer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  xbeizer
+//
+//  Created by 健太 原 on 2/10/12.
+//  Copyright (c) 2012 Tokyo Tech High School of Science and Technoligy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
