880e3367c77f87ce53f8283e33201739fde3ff14
mactkg <mactkg@gmail.com>
2012-02-12 05:18:23 +0900

    add header file

---
+++ xbeizer/xbeizer.h
@@ -0,0 +1,18 @@
+//
+//  xbeizer.h
+//  
+//
+//  Created by 健太 原 on 2/10/12.
+//  Copyright (c) 2012 Tokyo Tech High School of Science and Technoligy. All rights reserved.
+//
+
+#ifndef _xbeizer_h
+#define _xbeizer_h
+
+#include <stdio.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/cursorfont.h>
+
+#endif
